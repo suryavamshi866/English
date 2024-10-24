e836e9b2c11e67bf09634701bcb0934f990b58da
suryavamshi866 <suryavamshi159@gmail.com>
2024-10-24 07:06:21 +0530

    Update ResetPassword.jsx

--- src/ResetPassword.jsx
+++ src/ResetPassword.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ResetPassword = () => {
     const [newPassword, setNewPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [message, setMessage] = useState('');
+    const navigate = useNavigate();
 
     const handleReset = () => {
         if (newPassword === confirmPassword) {
-            
+            // Backend password reset logic will go here
             setMessage('Password reset successfully!');
+            console.log("Hi");
+            navigate('/'); 
         } else {
             setMessage('Passwords do not match!');
         }
